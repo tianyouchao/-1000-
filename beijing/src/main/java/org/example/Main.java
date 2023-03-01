8cc158b36c6a756e0d60b9866ac18023c29108c1
tianyouchao <126130019+tianyouchao@users.noreply.github.com>
2023-03-01 19:54:54 +0800

    Add files via upload

---
+++ beijing/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+
+
+            public static void main(String args[]) throws Exception
+
+            {
+                Scanner sum=new Scanner(System.in);
+                double a=sum.nextInt(),b=sum.nextInt();
+                System.out.println(a+b);
+            }
+
+
+
+}
